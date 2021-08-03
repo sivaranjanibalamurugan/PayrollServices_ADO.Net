057173336cc4a3c46500bb43ac2460c2f304ecf3
unknown <sivaranjanib99@gmail.com>
2021-08-03 20:53:25 +0530

    [siva]UC1 creating a database using c#

---
+++ PayrollServices/EmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollServices
+{
+    class EmployeeRepository
+    {
+        public static string connectionString = "Data Source=(localdb)\\ProjectsV13;Initial Catalog=Payroll_services";      
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+        public void GetAllData()
+        {
+
+        }
+    }
+}
